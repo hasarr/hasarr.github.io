423d2eca35e485b8706c467a862e342ea20da1e7
hasarr <ihasarpl@gmail.com>
2020-09-06 02:14:11 +0200

    Animated mobile menu fix

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,14 +1,12 @@
-const mobileIcon = document.querySelector(".mobileMenuIcon");
 const mobileMenu = document.querySelector(".mobileMenu");
 const dropMenu = document.getElementById("mobileDropList");
 
-mobileIcon.addEventListener("click", () => {
-  mobileMenu.classList.toggle("open");
-});
-mobileMenu.addEventListener("click", () => {
-  mobileMenu.classList.toggle("open");
-});
-function showDropMenu()
+
+function toggleDropMenu()
 {
-        dropMenu.style.display = "block";
+    dropMenu.classList.toggle("open");
+}
+function toggleMenu()
+{
+    mobileMenu.classList.toggle("open");
 }
