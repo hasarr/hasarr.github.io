9b58b4eb537f6ba6fc320afedaf6de3afd0bfab1
hasarr <ihasarpl@gmail.com>
2020-09-12 14:19:46 +0200

    google tags and ie not supported info

--- scripts/browserDetect.js
+++ scripts/browserDetect.js
@@ -1,6 +1,6 @@
-function browserDetect()
+function redirect()
 {
-    var isIE = /*@cc_on!@*/false || !!document.documentMode; //is internet explorer?
-    if(isIE == true)location.href="not_supported.html";
-
+        var isIE = /*@cc_on!@*/false || !!document.documentMode; //is internet explorer?
+        if(isIE == true)location.href="not_supported.html";
 }
+window.onload=redirect;
